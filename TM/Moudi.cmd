374c2bc1baeeaf26127cae6a3111899c5273ce62
Norton Allen <allen@huarp.harvard.edu>
2023-01-18 15:15:19 -0500

    Restart changes

--- TM/Moudi.cmd
+++ TM/Moudi.cmd
@@ -1,4 +1,18 @@
+%{
+  #ifdef SERVER
+    void create_restart_file() {
+      FILE *fp = fopen("restart.txt", "w");
+      fprintf(fp, "Restart, please\n");
+      fclose(fp);
+    }
+  #endif
+%}
+
 %INTERFACE(Tx) <UDPCmdTx>
 &command
   : &^command
+  ;
+
+&^command
+  : Create Restart File * { create_restart_file(); }
   ;
