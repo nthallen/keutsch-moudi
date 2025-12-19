443cd005921f9129b9e8902e5d580678f5fea4ad
Norton Allen <allen@huarp.harvard.edu>
2025-12-19 12:50:52 -0500

    Saving changes before losing laptop

--- eng/gmoudiahk.m
+++ eng/gmoudiahk.m
@@ -1,5 +1,5 @@
 function fig = gmoudiahk(varargin);
 % gmoudiahk(...)
 % Alicat HK
-ffig = ne_group(varargin,'Alicat HK','pmoudiahks','pmoudiahkstale','pmoudiahkt','pmoudiahkmbar');
+ffig = ne_group(varargin,'Alicat HK','pmoudiahks','pmoudiahkstale','pmoudiahkmbar','pmoudiahkt');
 if nargout > 0 fig = ffig; end
--- eng/gmoudialicat.m
+++ eng/gmoudialicat.m
@@ -1,5 +1,5 @@
 function fig = gmoudialicat(varargin);
 % gmoudialicat(...)
 % Alicat
-ffig = ne_group(varargin,'Alicat','pmoudialicatccm','pmoudialicatnccm');
+ffig = ne_group(varargin,'Alicat','pmoudialicatnccm','pmoudialicatccm');
 if nargout > 0 fig = ffig; end
